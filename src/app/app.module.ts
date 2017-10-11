13abf92be92727e85294959ff7e066010327a0bc
kalekarnn <kalekar.narendra@gmail.com>
2017-10-11 09:53:10 +0530

    initial commit with UI only

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NewsCardComponent } from './news-card/news-card.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsCardComponent,
+    NavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
